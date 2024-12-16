0d0e4d54f18698137218913192fa26a051eb0ac6
Kunal <choudharykunal646@gmail.com>
2024-12-16 16:05:06 +0530

    all files commited

---
+++ Runner.java
@@ -0,0 +1,16 @@
+package LinkedLists;
+
+public class Runner {
+    public static void main(String[] args) {
+        LinkedList list=new LinkedList();
+        list.insert(10);
+        list.insert(16);
+        list.insert(11);
+        list.insert(1);
+        list.insert(12);
+        list.show();
+        
+    }
+    
+    
+}
